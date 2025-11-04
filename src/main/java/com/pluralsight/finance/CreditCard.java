a8d2f2ebe4acf47fdac82a5a6ac5b8fe9feb803c
Mateo Morales <Moralmat76@gmail.com>
2025-11-04 12:38:36 -0600

    implementing Classes

--- src/main/java/com/pluralsight/finance/CreditCard.java
+++ src/main/java/com/pluralsight/finance/CreditCard.java
@@ -5,6 +5,17 @@ public class CreditCard extends Account {
         super(name, accountNumber, balance);
     }
 
+    public  double charge(double amount){
+        double newBalance = getBalance() + amount;
+        setBalance(newBalance);
+        return newBalance;
+    }
+    public double pay(double amount){
+        double newBalance = getBalance() - amount;
+        setBalance(newBalance);
+        return newBalance;
+    }
+
     @Override
     public double getValue() {
         return getBalance();
--- src/main/java/com/pluralsight/finance/Portfolio.java
+++ src/main/java/com/pluralsight/finance/Portfolio.java
@@ -1,4 +1,16 @@
 package com.pluralsight.finance;
 
-public class Portfolio {
+import java.util.ArrayList;
+
+public abstract class Portfolio {
+    private String name;
+    private String owner;
+    private ArrayList<Valuable> assets;
+
+    public Portfolio(String name, String owner, ArrayList<Valuable> assets) {
+        this.name = name;
+        this.owner = owner;
+        this.assets = assets;
+    }
+    public abstract double getValue();
 }

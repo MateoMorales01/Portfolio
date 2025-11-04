b26382a5e0ea83fdf691259b8d97acf80874cd33
Mateo Morales <Moralmat76@gmail.com>
2025-11-04 14:19:35 -0600

    Test Portfolio

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,7 +1,21 @@
 package com.pluralsight;
 
+import com.pluralsight.finance.CheckingAccount;
+import com.pluralsight.finance.CreditCard;
+import com.pluralsight.finance.Gold;
+import com.pluralsight.finance.Portfolio;
+
 public class Application {
     public static void main(String[] args) {
+        CheckingAccount checkingAccount = new CheckingAccount("Melvin's Checking", "33681",368);
+        CreditCard creditCard = new CreditCard("Melvin's Credit", "48790", 12000);
+        Gold gold = new Gold("Melvin's Gold",4000, 12);
 
+        Portfolio portfolio = new Portfolio("Melvin's Portfolio", "Melvin");
+
+        portfolio.add(checkingAccount);
+        portfolio.add(creditCard);
+        portfolio.add(gold);
+        System.out.println(portfolio.getValue());
     }
 }
